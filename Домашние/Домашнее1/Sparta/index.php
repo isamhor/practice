f9bf9913330a69fddb4cd4a083fbaf1e6e62ee91
Igosam <ihor.samokish@gmail.com>
2016-05-29 16:11:47 +0300

    First commit

---
+++ Домашние/Домашнее1/Sparta/index.php
@@ -0,0 +1,21 @@
+<?php
+$test = 550;
+switch ($test) {
+case 20:
+echo 'good';
+break;
+
+case 30:
+echo 'excellent';
+break;
+
+case 50:
+echo 'too far';
+break;
+
+default :
+echo 'okay';
+break;
+}
+?>
+
